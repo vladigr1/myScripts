a1ec817c357cf36a1892bcb34aa720359fee1729
vlad <vladigr1@gmail.com>
2020-10-16 16:03:19 +0300

    delete output example and rename some stuff

--- ynews.sh
+++ ynews.sh
@@ -2,4 +2,4 @@
 
 #script to read news like a human
 /c/Users/vladi/source/myScripts/share/news/Scripts/python.exe /c/Users/vladi/source/myScripts/share/news/ynet-news.py
-/c/Program\ Files/Mozilla\ Firefox/firefox.exe file:///C:/Users/vladi/source/myScripts/share/news/html-output/news.html &>/dev/null
+/c/Program\ Files/Mozilla\ Firefox/firefox.exe file:///C:/Users/vladi/source/myScripts/share/news/html-output/news.html &>/dev/null &
